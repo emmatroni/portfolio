86a733f1c52a2eb3f8e1e1e51504f1629d9b4852
emmatroni <66473121+emmatroni@users.noreply.github.com>
2023-04-30 15:29:35 +0200

    boh

--- animation/img-animation/slider.js
+++ animation/img-animation/slider.js
@@ -1,48 +1,45 @@
-// FIRST interaction: pressing mouse down
-// listening to any mouse down event that may occur
-// when it appens I keep track of the exact X position of the mouse
-// and we use it as the starting point of the slider
-// to store that value we create a new custom value on our track element
-// it will update everytime the mouse is down
-// we need to store the relative percentage everytime the mouse is released so it doesn't restart from 0 every time
-
 const track = document.getElementById("image-track");
 
-window.onmousedown = e => {
-    track.dataset.mouseDownAt = e.clientX;
-    track.dataset.prevPercentage = track.dataset.percentage;
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
 }
 
-window.onmouseup = () => {
-    track.dataset.mouseDownAt = "0";
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
 }
 
-// SECOND: mouse move listener
-// relative position from starting point + end slider
-// we devide the two of them to obtain the percentage of the slider moved
-window.onmousemove = e => {
-    if(track.dataset.mouseDownAt === "0") return;
+/* -- Had to add extra lines for touch events -- */
 
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth /2;
+window.onmousedown = e => handleOnDown(e);
 
-    const percentage = (mouseDelta /maxDelta) * -100,
-        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+window.ontouchstart = e => handleOnDown(e.touches[0]);
 
-    track.dataset.percentage = nextPercentage;
-    
-    //track.style.transform = `translate(${nextPercentage}%, -50%)`;
-    track.animate({
-        transform:  `translate(${nextPercentage}%, -50%)`
-    }, { duration: 1200, fill: "forwards"});
+window.onmouseup = e => handleOnUp(e);
 
-    Math.min(nextPercentage, 0);
-    Math.max(nextPercentage, -100);
+window.ontouchend = e => handleOnUp(e.touches[0]);
 
-    for(const image of track.getElementsByClassName("image")){
-        //image.style.objectPosition = `${nextPercentage + 100}% 50%`;
-        image.animate({
-            objectPosition: `${nextPercentage + 100}% center`
-        }, { duration:1200, fill:"forwards"});
-    }
-}
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
